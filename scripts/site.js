7bf31a28ddc5dce60fd5aed087a06fa496116ef4
andrearaykova <a.p.raykova@gmail.com>
2019-08-11 11:07:00 +0300

    Remove some js

--- scripts/site.js
+++ scripts/site.js
@@ -1,7 +1,3 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     document.getElementsByTagName('body')[0].classList.add('loaded');
 });
-
-if (!("ontouchstart" in document.documentElement)) {
-    document.documentElement.className += " no-touch";
-}
